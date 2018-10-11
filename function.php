d907ecddec33465ba952ae3a78035fbc7a8a24ad
Lucio Ticali <infofreewebsolution.it>
2018-10-11 16:10:41 +0200

    Rimosso commenti e funzioni

--- function.php
+++ function.php
@@ -1,5 +1,4 @@
 <?php
-/*File realizzato appositamente per il corso di git hub base da Lucio e Mattia*/
 
 /*addizione*/
 
@@ -13,23 +12,7 @@ function sottrazione (n1,n2){
 	return n1-n2;
 }
 
-/*moltiplicazione*/
-
-function moltiplicazione (n1,n2){
-	return n1*n2;
-}
 
 
-
-/*divisione*/
-
-function divisione (n1,n2){
-
-	if( n2 !== 0){
-	
-	return n1*n2;
-	}else{
-		return "Error";
-}
 
 ?>
